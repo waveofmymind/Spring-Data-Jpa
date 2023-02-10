92d978fbe41a0f800bb6c18e3c0edc69dee5e8a4
waveofmymind <sjun0913@gmail.com>
2023-02-10 15:02:05 +0900

    Initial commit

---
+++ src/test/java/wave/datajpa/DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package wave.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
